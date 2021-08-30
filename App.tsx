291f993553d8e8aaf0e42f8e25b7a7b27b59f4fb
Daniel Marques <daniel.silva.city@gmail.com>
2021-08-29 22:08:19 -0300

    Integration tab navigation

--- App.tsx
+++ App.tsx
@@ -9,11 +9,11 @@ import {
   Poppins_700Bold,
 } from '@expo-google-fonts/poppins';
 
-import theme from './src/global/styles/theme';
+import { NavigationContainer } from '@react-navigation/native';
 
-import { Dashboard } from './src/screens/Dashboard';
-import { Register } from './src/screens/Register';
-import { CategorySelect } from './src/screens/CategorySelect';
+import { AppRoutes } from './src/routes/app.routes';
+
+import theme from './src/global/styles/theme';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -28,7 +28,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
